1cbdfaf266c40bb0ae711cf7a4c543f3a7f20460
alexisbonillamartinez04 <alexis.bonilla.5cm@gmail.com>
2024-05-28 10:54:52 -0600

    Update main.js

--- control/web/js/main.js
+++ control/web/js/main.js
@@ -12,7 +12,7 @@ function callApi(estatus) {
   // Hacer una petición para un usuario con ID especifico
   axios
     .get(
-      "https://3.21.41.173/iot-car/back-end/apis/setRegistro.php?valorEstatus=" +
+      "https://3.21.127.66/iot-car/back-end/apis/setRegistro.php?valorEstatus=" +
         estatus
     )
     .then(function (response) {
